a1deb8749ce94091b18ede2e8b099445acb6492a
FernandoRen <jesussoriaf27@gmail.com>
2022-11-30 01:30:06 -0600

    adding my changes again

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,39 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private router: Router
+  ) {
+    this.validateToken();
+  }
+
+  validateToken(){
+    const validateToken = localStorage.getItem('token');
+    if (validateToken !== null && validateToken !== undefined) {
+      this.router.navigateByUrl('/create');
+    } else {
+      this.router.navigateByUrl('/home');
+    }
+  }
+
+  closeSession(){
+    Swal.fire({
+      title: '¿Estás seguro?',
+      text: "Se cerrará la presente sesión, tendrás que autenticarte nuevamente",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, cerrar sesión',
+      cancelButtonText: 'Cancelar',
+      heightAuto: false
+    }).then((result) => {
+      if (result.isConfirmed) {
+        localStorage.removeItem("token");
+        this.router.navigateByUrl('/home');
+      }
+    });
+  } 
 }
+
+
--- src/app/auth/register/register.page.ts
+++ src/app/auth/register/register.page.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AlertController, LoadingController, ToastController } from '@ionic/angular';
 import { AuthService } from '../auth.service';
 import { User } from '../user.model';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -45,23 +46,68 @@ export class RegisterPage  {
   });
 
   async onSubmit(){
-    const loading = await this.loadingCtrl.create({ message: 'Registering...' });
-    await loading.present();
-    this.authService.register(this.user).subscribe(
-      // If success
-      async () => {
-        const toast = await this.toastCtrl.create({ message: 'User Created', duration: 2000, color: 'dark' });
-        await toast.present();
-        loading.dismiss();
-        this.form.reset();
-      },
-      // If there is an error
-      async () => {
-        const alert = await this.alertCtrl.create({ message: 'There is an error', buttons: ['OK'] });
-        loading.dismiss();
-        await alert.present();
-      }
-    );
+    if (!this.functionToValitePasswordFormat(this.user.password)) {
+      this.validatingPassword();
+    } else {
+      const loading = await this.loadingCtrl.create({ message: 'Registering...' });
+      await loading.present();
+      this.authService.register(this.user).subscribe(
+        // If success
+        async () => {
+          const toast = await this.toastCtrl.create({ message: 'User Created', duration: 2000, color: 'dark' });
+          await toast.present();
+          loading.dismiss();
+          this.form.reset();
+        },
+        // If there is an error
+        async () => {
+          const alert = await this.alertCtrl.create({ message: 'There is an error', buttons: ['OK'] });
+          loading.dismiss();
+          await alert.present();
+        }
+      );
+    }
+  }
+
+  validatingPassword(){
+    let showMessage = document.getElementById("wrong-password"); //variable to show a message in case there was an error
+    Swal.fire({
+      title: "Error", 
+      text: "Contraseña inválida, por favor ingrese una contraseña válida", 
+      icon: "error", 
+      heightAuto: false
+    });
+    showMessage.innerHTML = `
+          <ul>
+              <li>Mínimo 8 caracteres</li>
+              <li>Máximo 16 caracteres</li>
+              <li>Al menos una letra mayúscula</li>
+              <li> Al menos una letra mínuscula</li>
+              <li>Al menos un número</li>
+              <li>Al menos 1 caracter especial que esté entre los siguientes @$?¡!*#/-_</li>
+          </ul>
+    `;
+    setTimeout(() => {
+      showMessage.innerHTML = ``;
+    }, 10000);
+  }
+
+  functionToValitePasswordFormat(password) {
+    //let passRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+    let passRegex = /^(?=(?:.*\d){1})(?=(?:.*[A-Z]){1})(?=(?:.*[a-z]){1})(?=(?:.*[@$?¡!*#/\-_]){1})\S{8,16}$/;
+    /*
+        Minimo 8 caracteres
+        Maximo 16 caracteres
+        Al menos una letra mayúscula
+        Al menos una letra minuscula
+        Al menos un dígito
+        Al menos 1 caracter especial
+    */
+    if (passRegex.test(password)) {
+        return true;
+    } else {
+        return false;
+    }
   }
 
 }
